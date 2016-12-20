4bb5a1490f1d9191a5099a5ac0281d56dc40b2ea
rayGreycloud <rayGreycloud@gmail.com>
2016-12-20 12:38:48 -0800

    Add event handlers to update progress bar, adjust volume or playback rate, and change current time when progress bar clicked

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Grab Elements
+// Grab video player elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -6,7 +6,6 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-
 
 // Build out functions
 function togglePlay() {
@@ -16,27 +15,49 @@ function togglePlay() {
 }
 
 function updateButton() {
+  // Another ternary to set play button appearance
   const icon = this.paused ? '►' : '❚ ❚';
   toggle.textContent = icon;
 }
 
 function skip() {
-  let message = '';
+  // Grab skip interval and convert to number
   const interval = parseFloat(this.dataset.skip);
-  if (interval > 0) {
-    message = 'Fast-Forwarding 25 seconds...';
-  } else {
-    message = 'Rewinding 10 seconds...';
-  }
-
-  console.log(message);
+  // Add it to current time index
   video.currentTime += interval;
 }
 
+function handleRangeUpdate() {
+  // change volume or playback rate when slider moved
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(event) {
+  const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(event);
+}
+
 // Hook up event listeners
+// Toggle play function when video clicked
 video.addEventListener('click', togglePlay);
+// Toggle play button to match video status
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-
+// Update progress bar
+video.addEventListener('timeupdate', handleProgress);
+// Toggle play function when play button clicked
 toggle.addEventListener('click', togglePlay);
+// Trigger skip function when either skip button clicked
 skipButtons.forEach(button => button.addEventListener('click', skip));
+// Trigger volume or playback rate change when range changed
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+// Trigger volume or playback rate change when mouse moves range slider
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+//
+progress.addEventListener('click', scrub);
